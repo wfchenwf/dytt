80ff7255d9a10b6f9fa7cc964ddf64c9ce891d05
winfred <45781437+wfchenwf@users.noreply.github.com>
2019-09-25 19:35:58 +0800

    Update main.py
    增加了将爬取数据存储到Mysql函数

--- main.py
+++ main.py
@@ -57,11 +57,89 @@ def startSpider():
         if TaskQueue.isMiddleQueueEmpty():
             break
         else:
-            pass
+            SaveMysql()
 
-    insertData()
+#     insertData()
+    SaveMysql()
 
 
+def table_exists(conn,table_name):
+    sql = "show tables;"
+    conn.execute(sql)
+    tables = [conn.fetchall()]
+    table_list = re.findall('(\'.*?\')', str(tables))
+    table_list = [re.sub("'",'',each) for each in table_list]
+    if table_name in table_list:
+        return 1
+    else:
+        return 0
+
+
+def SaveMysql():
+    import pymysql
+    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password="123456", database='dytt', charset="utf8")
+
+    cursor = conn.cursor()
+
+    CreateTableSql = """
+        Create Table lastest_moive(
+            m_id int not null auto_increment,
+            m_type varchar(100),
+            m_trans_name varchar(200),
+            m_name varchar(100),
+            m_decade varchar(30),
+            m_conutry varchar(30),
+            m_level varchar(100),
+            m_language varchar(30),
+            m_subtitles varchar(100),
+            m_publish varchar(30),
+            m_IMDB_socre varchar(50),
+            m_douban_score varchar(50),
+            m_format varchar(20),
+            m_resolution varchar(20),
+            m_size varchar(10),
+            m_duration varchar(10),
+            m_director varchar(50),
+            m_actors varchar(1000),
+            m_placard varchar(200),
+            m_screenshot varchar(200),
+            m_ftpurl varchar(200),
+            m_dytt8_url varchar(200),
+            primary key (m_id)
+        );
+    """
+
+    InsertSql = '''
+           Insert into lastest_moive(m_type, m_trans_name, m_name, m_decade, m_conutry, m_level, m_language, m_subtitles, m_publish, m_IMDB_socre, 
+           m_douban_score, m_format, m_resolution, m_size, m_duration, m_director, m_actors, m_placard, m_screenshot, m_ftpurl,
+           m_dytt8_url)
+           values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+       '''
+
+    # if not cursor.execute(SelectSql).fetchone():
+    #     cursor.execute(CreateTableSql)
+    #     conn.commit()
+    #     print('====  创建表成功  ====')
+    # else:
+    #     print('====  创建表失败, 表已经存在  ====')
+    table_name = 'lastest_moive'
+    if(table_exists(cursor, table_name) != 1):
+        cursor.execute(CreateTableSql)
+        conn.commit()
+        print('====  创建表成功  ====')
+    count = 1
+
+    # while not TaskQueue.isContentQueueEmpty():
+    item = TaskQueue.getContentQueue().get()
+    cursor.execute(InsertSql, Utils.dirToList(item))
+    conn.commit()
+    print('插入第 ' + str(count) + ' 条数据成功')
+    count = count + 1
+
+    conn.commit()
+    conn.close()
+
+    
 def insertData():
     DBName = 'dytt.db'
     db = sqlite3.connect('./' + DBName, 10)
